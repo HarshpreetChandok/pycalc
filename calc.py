a72298282ed21ed87e3a529d81f21be367707585
Harshpreet Chandok <chandokharshpreet@gmail.com>
2019-07-18 10:42:21 -0400

    Add docstring to funtion

--- calc.py
+++ calc.py
@@ -1,4 +1,7 @@
 def compute(string)
+    """Perform simple arithmetic based on string input.
+	example: '5+7 -> 12'
+    """
     values = string.split('')
     num0 = int(values[0])
     num1 = int(values[1])
