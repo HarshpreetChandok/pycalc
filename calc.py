fd602cd35816f66be46baa5a324723a2847a4756
Harshpreet Chandok <chandokharshpreet@gmail.com>
2019-07-18 10:05:52 -0400

    Initial work on Py calculator

---
+++ calc.py
@@ -0,0 +1,10 @@
+def compute(string)
+    values = string.split('')
+    num0 = int(values[0])
+    num1 = int(values[1])
+    operator = values[1]
+    if operator == '+'
+        return num0 + num1
+    else:
+        print('unkmown operator!')
+	return 0
